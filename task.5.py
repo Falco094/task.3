eaa9f17b3c5b9eeb03875508d4fa0e52cfd1da72
Abdullrahman Khaddam <138903318+Falco094@users.noreply.github.com>
2024-06-26 11:35:43 +0100

    Update task.5.py

--- task.5.py
+++ task.5.py
@@ -1,14 +1,6 @@
-num = int(input(8))
-
-
-def countdown(num):
-    while num >= 0:
-        print(num)
-
-        num -= 1
-
-
-
-
-        
-countdown(8)
+# 0 1 2 3 4 range  (0:n)
+#4 3 2 1 0  range (n:0 : -1)
+def countdown (n):
+    for i in range (n , 0, -1 ):
+        print(i)
+        countdown(6)
